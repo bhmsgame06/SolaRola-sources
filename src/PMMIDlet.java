fe160b0780548fd4439fb9377332f4a9683a2fbd
bhmsgame06 <bhmsgamexbox2010@gmail.com>
2025-06-19 05:17:01 +0500

    Pause screen

--- src/PMMIDlet.java
+++ src/PMMIDlet.java
@@ -27,7 +27,7 @@ public class PMMIDlet extends MIDlet {
 		Game.Method29();
 		Game.Method31();
 		notifyPaused();
-		Game.Method261();
+		Game.setNoPauseScreen();
 	}
 
 	public final void destroyApp(boolean unconditional) {
